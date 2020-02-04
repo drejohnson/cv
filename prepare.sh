b162112f24b04dab0ca904d94c7f48fdd818362c
DeAndre Johnson <dre@phreshr.com>
2020-02-05 05:46:24 +0800

    update resume

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,5 @@
 
 echo preparing...
-hackmyresume build resume.json TO build/index.all -t ../fresh-theme-positive/
-cp build/index.html index.html
+hackmyresume build resume.json TO build/resume.all -t positive
+cp build/resume.html index.html
 echo done.
